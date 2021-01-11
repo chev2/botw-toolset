007fa6ff42115065a1bf82e6ca931fde145047f5
Chev <11602755+chev2@users.noreply.github.com>
2021-01-10 20:07:53 -0800

    Remove Color class, add InvalidMagicException

---
+++ botw-toolset/Exceptions/InvalidMagicException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BOTWToolset.Exceptions
+{
+    /// <summary>
+    /// An exception that's thrown when the magic header in a file is not what was expected.
+    /// </summary>
+    class InvalidMagicException : Exception
+    {
+        public InvalidMagicException() : base() { }
+        public InvalidMagicException(string message) : base(message) { }
+        public InvalidMagicException(string message, Exception inner) : base(message, inner) { }
+    }
+}
