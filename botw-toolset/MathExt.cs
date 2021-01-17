a9639a43b3087f1967384d96771e0e4cf9ff73e1
Chev <11602755+chev2@users.noreply.github.com>
2021-01-16 21:16:38 -0800

    MathExt.Lerp method

--- botw-toolset/MathExt.cs
+++ botw-toolset/MathExt.cs
@@ -21,5 +21,22 @@ namespace BOTWToolset
             else if (val.CompareTo(max) > 0) return max;
             else return val;
         }
+
+        /// <summary>
+        /// Linearly interpolates a value.
+        /// </summary>
+        /// <param name="from">Value to interpolate.</param>
+        /// <param name="to">Value to interpolate to.</param>
+        /// <param name="delta">Amount to interpolate - should be between 0 and 1.</param>
+        /// <returns></returns>
+        public static float Lerp(float from, float to, float delta)
+        {
+            if (delta > 1)
+                return to;
+            if (delta < 0)
+                return from;
+
+            return from + (to - from) * delta;
+        }
     }
 }
